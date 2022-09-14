ca312889b88c7916fffc51a8b370d397046e3673
Chaparron <chaparroncoder@gmail.com>
2022-09-14 08:46:41 +0200

    todo funcionando en consola

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,171 +1,252 @@
-package tasklist
-// import kotlinx.datetime.*
+import kotlinx.datetime.*
+import com.squareup.moshi.*
 
-fun main() {
-    val list = mutableListOf(mutableListOf<String>())
-    list.clear()
-    askInput(list)
-
+enum class Priority(val humanStr: String, color: String) {
+    C("Critical","\u001B[101m \u001B[0m"),
+    H("High","\u001B[103m \u001B[0m"),
+    N("Normal","\u001B[102m \u001B[0m"),
+    L("Low","\u001B[104m \u001B[0m")
 }
 
-fun askInput(list: MutableList<MutableList<String>>) {
-    println("Input an action (add, print, end):")
-    when (readln().lowercase()) {
-        "add" -> addSubList(list)
-        "print" -> displayTasks(list)
-        "end" -> println("Tasklist exiting!")
-        else -> println("The input action is invalid").also { askInput(list) }
-    }
+enum class Field {
+    Priority, Date, Time, Task
 }
 
-fun addSubList(list: MutableList<MutableList<String>>) {
-    val priority = setPriority()
-    val date = setDate()
-    val time = setTime()
-    println("Input a new task (enter a blank line to end):")
-    val subList = addToDo()
-    if (subList.isNotEmpty()) subList.add(0,"$date $time $priority").also { list.add(subList) } else println("The task is blank")
-    askInput(list)
-}
-
-fun setPriority(): String {
-    println("Input the task priority (C, H, N, L):")
-    val myRegex = "[CHNLchnl]".toRegex() // WTF why green??
-    val priority = readln().uppercase()
-    return if (priority.matches(myRegex)) priority else setPriority()
-}
-
-fun wrongDate(msg: String) {
-    println("The input date is invalid")
-    if ("victor".length == "hacker".length) println("\n$msg\n")
-    setDate()
-}
-fun checkYear(year: String): String {
-    var yearFunction = year
-    when (yearFunction.length){
-        4 -> yearFunction = year
-        3 -> yearFunction = "0$year"
-        2 -> yearFunction = "00$year"
-        1 -> yearFunction = "000$year"
-        else -> wrongDate("$year lenght no está entre 1 y 4, year.length es: ${year.length}")
+class Task(var priority: Priority, var dateTime: LocalDateTime) {
+    var list = mutableListOf<String>()
+    fun add(s: String) {
+        list.add(s)
     }
-    val yearRegex = "\\d\\d\\d\\d".toRegex()
-    if(!yearFunction.matches(yearRegex)) wrongDate("$yearFunction - El año no pasa el Regex")
-    return yearFunction
-}
-fun checkMonth(month: String): String {
-    var monthFunction = month
-    when (monthFunction.length){
-        2 -> monthFunction = month
-        1 -> monthFunction = "0$month"
-        else -> wrongDate("$month - el mes no tiene ni 1 ni 2 cifras, tiene: ${month.length}")
+    fun dueTag(): String {
+        val now = Clock.System.todayAt(TimeZone.UTC)
+        val taskDate = dateTime.toString().split("T")[0].toLocalDate()
+        return if (now > taskDate) {
+            "\u001B[101m \u001B[0m" //"O" OutDated
+        } else if (now == taskDate) {
+            "\u001B[103m \u001B[0m" // "T" Today
+        } else {
+            "\u001B[102m \u001B[0m" // "I" In time
+        }
+
     }
-    val monthRegex = "\\d\\d".toRegex()
-    if(!monthFunction.matches(monthRegex)) wrongDate("$monthFunction - el mes no pasa el Regex")
-    if (monthFunction.toInt() > 12 || monthFunction.toInt() < 1) wrongDate("$monthFunction - el día del mes no va del 1 al 12")
-
-    return monthFunction
-}
-fun checkDay(day: String, month: String): String {
-    var dayFunction = day
-    when (day.length){
-        2 -> dayFunction = day
-        1 -> dayFunction = "0$day"
-        else -> wrongDate("$day - el día no tiene ni una ni dos cifras, tiene: ${day.length}")
-    }
-    val dayRegex = "\\d\\d".toRegex()
-    if(!dayFunction.matches(dayRegex)) wrongDate("$dayFunction - el dia no pasa el Regex")
-    when (month.toInt()) {
-        1,3,5,7,8,10,12 -> if (dayFunction.toInt() > 31) wrongDate("$month-$dayFunction - tiene más de 31 días")
-        4, 6, 9, 11 -> if (dayFunction.toInt() > 30) wrongDate("$month-$dayFunction - tiene más de 30 días y es el mes 4,6,9 o 11")
-        2 -> if (dayFunction.toInt() > 28) wrongDate("$month-$dayFunction - tiene más de 28 días y es febrero")
-    }
-    return dayFunction
-}
-fun setDate(): String {
-
-    var year = ""
-    var month = ""
-    var day = ""
-
-    println("Input the date (yyyy-mm-dd):")
-
-    var date = readln()
-
-    val dateToArray = date.split("-")
-    if(dateToArray.size == 3) {
-        year = dateToArray[0]
-        month = dateToArray[1]
-        day = dateToArray[2]
-    } else {
-        wrongDate("$year-$month-$day - dateToArray.size no es igual a 3 es ${dateToArray.size}")
+    fun printDateTime(): String {
+        return dateTime.toString().split("T").joinToString(" | ")
     }
 
-    year = checkYear(year)
-    month = checkMonth(month)
-    day = checkDay(day, month)
-    date = ("$year-$month-$day")
-    return date
-}
-
-fun setTime(): String {
-    var hours = ""
-    var minutes = ""
-
-    fun wrongTime() {
-        println("The input time is invalid")
-        setTime()
-    }
-
-    println("Input the time (hh:mm):")
-
-
-    var time = readln()
-    val timeToArray = time.split(":")
-    if (timeToArray.size == 2) {
-        hours = timeToArray[0]
-        minutes = timeToArray[1]
-    } else wrongTime()
-
-    when (hours.length){
-        2 -> hours = hours  // y si no te igualo como digo que no te pase nada?
-        1 -> hours = "0$hours"
-        else -> wrongTime()
-    }
-    when (minutes.length){
-        2 -> minutes = minutes
-        1 -> minutes = "0$minutes"
-        else -> wrongTime()
-    }
-    time = ("$hours:$minutes")
-    val myRegex = "^[0-2][0-3]:[0-5][0-9]\$".toRegex() // porque coño el Regex es amarillo
-    if (!time.matches(myRegex)) {
-        wrongTime()
-    }
-    return time
-}
-
-fun addToDo(): MutableList<String> {
-    val subList = mutableListOf<String>()
-    while (true) {
-        val todo = readln().trim()
-        if (todo.isNotEmpty()) subList.add(todo) else break
-    }
-    return subList
-}
-
-fun displayTasks(list : MutableList<MutableList<String>>) {
-    if (list.isEmpty()) {
-        println("No tasks have been input")
-    } else {
-        for ((i,subList) in list.withIndex()) {
-            val listNumber = i + 1
-            print(listNumber.toString() + if (listNumber >= 10) " " else "  ")
-            for ((ind, todo) in subList.withIndex()){
-                if (ind == 0) println(todo) else println("   $todo")
-            }
-            println()
+    fun getColor(): String{
+        return when (priority.toString()) {
+            "C" -> "\u001B[101m \u001B[0m"
+            "H" -> "\u001B[103m \u001B[0m"
+            "N" -> "\u001B[102m \u001B[0m"
+            "L" -> "\u001B[104m \u001B[0m"
+            else -> "you broke it"
         }
     }
-    askInput(list)
+    fun editPriority(newPriority: Priority) {
+        priority = newPriority
+    }
+    fun editDate(year: Int, month: Int, day: Int) {
+        dateTime = LocalDateTime(year, month, day, dateTime.hour, dateTime.minute)
+    }
+    fun editTime(hour: Int, minute: Int) {
+        dateTime = LocalDateTime(dateTime.year, dateTime.monthNumber, dateTime.dayOfMonth, hour, minute)
+    }
+    fun editList( newList: MutableList<String>) {
+        list = newList
+    }
+}
+
+fun main() {
+    val taskList = mutableListOf<Task>()
+
+    while (true) {
+        println("Input an action (add, print, edit, delete, end):")
+        val input = readln()
+        when (input.lowercase()) {
+            "add" -> addToList(taskList)
+            "print" -> printList(taskList)
+            "delete" -> deleteList(taskList)
+            "edit" -> editList(taskList)
+            "end" -> {
+                println("Tasklist exiting!")
+                break
+            }
+
+            else -> println("The input action is invalid")
+        }
+    }
+}
+
+private fun addToList(taskList: MutableList<Task>) {
+    val priority = readPriority()
+    val date = readDate()
+    val time = readTime()
+
+    val task = Task(priority, LocalDateTime(date.year, date.month, date.dayOfMonth, time.hour, time.minute))
+
+    println("Input a new task (enter a blank line to end):")
+    while (true) {
+        val input = readln().trimIndent()
+        if (input.isEmpty()) {
+            break
+        } else {
+            task.add(input)
+        }
+    }
+
+    if (task.list.isEmpty()) {
+        println("The task is blank")
+    } else {
+        taskList.add(task)
+    }
+}
+
+private fun readPriority(): Priority {
+    while (true) {
+        try {
+            println("Input the task priority (C, H, N, L):")
+            return Priority.valueOf(readln().uppercase())
+        } catch (e: IllegalArgumentException) {
+            // just try again without saying anything because jetbrains says so
+        }
+    }
+}
+
+private fun readDate(): LocalDate {
+    while (true) try {
+        println("Input the date (yyyy-mm-dd):")
+        val (y, m, d) = readln().split("-").map { it.toInt() }
+        return LocalDate(y, m, d)
+    } catch (e: Exception) {
+        println("The input date is invalid")
+    }
+}
+
+private fun readTime(): LocalDateTime {
+    while (true) try {
+        println("Input the time (hh:mm):")
+        val (h, m) = readln().split(":").map { it.toInt() }
+        return LocalDateTime(2000, 1, 1, h, m)
+    } catch (e: Exception) {
+        println("The input time is invalid")
+    }
+}
+
+private fun stillNoTasks () {
+    println("No tasks have been input")
+}
+private fun printList(taskList: MutableList<Task>) {
+    if (taskList.isEmpty()) {
+        return stillNoTasks()
+    }
+    println("+----+------------+-------+---+---+--------------------------------------------+")
+    println("| N  |    Date    | Time  | P | D |                   Task                     |")
+    println("+----+------------+-------+---+---+--------------------------------------------+")
+    taskList.forEachIndexed { index, task ->
+        print(
+            "| ${(index + 1).toString().padEnd(2)} | ${task.printDateTime()} | ${task.getColor()} | ${task.dueTag()} |"
+        )
+        for (i in 0 until task.list.size) {
+            var arrayTask = task.list[i].toString().chunked(44)
+            if (i == 0){
+                for (ind in 0 until arrayTask.size){
+                    if (ind == 0) {
+                        print(arrayTask[0].padEnd(44,' ') + "|\n")
+                    } else {
+                        print("|    |            |       |   |   |${arrayTask[ind].padEnd(44,' ')}|\n")
+                    }
+                }
+            } else {
+                for (ind in 0 until arrayTask.size) {
+                    print("|    |            |       |   |   |${arrayTask[ind].padEnd(44,' ')}|\n")
+                }
+            }
+        }
+        println("+----+------------+-------+---+---+--------------------------------------------+")
+    }
+    println()
+}
+
+private fun deleteList(taskList: MutableList<Task>) {
+    if (taskList.size == 0) {
+        return stillNoTasks()
+    }
+    printList(taskList)
+    var selectedIndex: Int?
+    outer@while (true) {
+        println("Input the task number (1-${taskList.size}):")
+        selectedIndex = readln().toIntOrNull()
+        if (selectedIndex == null || selectedIndex > taskList.size || selectedIndex < 1) {
+            println("Invalid task number")
+            continue@outer
+        } else break@outer
+    }
+    taskList.removeAt(selectedIndex!! - 1)
+    println("The task is deleted")
+}
+
+private fun selectField():Field {
+    while (true) try {
+        println("Input a field to edit (priority, date, time, task):")
+        return Field.valueOf(readln().replaceFirstChar{it.uppercase()})
+    } catch (e: Exception) {
+        println("Invalid field")
+    }
+}
+private fun editList(taskList: MutableList<Task>) {
+    if (taskList.size == 0) {
+        return stillNoTasks()
+    }
+    printList(taskList)
+    var selectedIndex: Int?
+    outer@while (true) {
+        println("Input the task number (1-${taskList.size}):")
+        selectedIndex = readln().toIntOrNull()
+        if (selectedIndex == null || selectedIndex > taskList.size || selectedIndex < 1) {
+            println("Invalid task number")
+            continue@outer
+        }
+        break@outer
+    }
+    val selectedTaskList = taskList[selectedIndex!! - 1]
+    val field = selectField().toString()
+    when (field) {
+        "Priority" -> {
+            val priority = readPriority()
+            selectedTaskList.editPriority(priority)
+        }
+        "Date" -> {
+            val date = readDate()
+            selectedTaskList.editDate(date.year, date.monthNumber, date.dayOfMonth)
+        }
+        "Time" -> {
+            val time = readTime()
+            selectedTaskList.editTime(time.hour, time.minute)
+        }
+        "Task" -> {
+//            val priority = taskList[selectedIndex!! - 1].priority
+//            val dateTime = taskList[selectedIndex!! - 1].dateTime
+
+
+            // fin del copia
+
+            val task = mutableListOf<String>()
+
+            println("Input a new task (enter a blank line to end):")
+            while (true) {
+                val input = readln().trimIndent()
+                if (input.isEmpty()) {
+                    break
+                } else {
+                    task.add(input)
+                }
+            }
+            selectedTaskList.editList(task)
+
+            // Fin del pega
+
+        }
+        else -> println("something went wrong")
+    }
+    println("The task is changed")
 }
